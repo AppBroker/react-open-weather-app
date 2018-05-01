da7bc33c2de41f040625522a4d52e0ff610ffb99
rmclaughlin <rmclaughlin@appbroker.biz>
2018-05-01 23:35:38 +0100

    Some additional comments: rmclaughlin <rmclaughlin@appbroker.biz>

--- reducers/reducer.js
+++ reducers/reducer.js
@@ -25,6 +25,12 @@ export const appInitialState = {
   searchData: null
 }
 
+/**
+  *  Decides the highest occuring pattern
+  *  @param {array} data - A list of all the weather patterns
+  *  @param {key} patternkey - Second level key structure on api json
+  *  @returns {String|Number} - The highest occuring pattern from the data
+  */
 export const getHighestOccurringPattern = (data, patternkey) => {
   const patternSpread = [...data]
   return chain(patternSpread.map((pattern) => {
@@ -32,6 +38,13 @@ export const getHighestOccurringPattern = (data, patternkey) => {
   })).countBy().toPairs().maxBy(last).head().value()
 }
 
+/**
+  *  Works out the average from any number based patterns:
+  *  @param {array} data - A list of all the weather patterns
+  *  @param {key} typekey - Second level key structure on api json
+  *  @param {key} patternkey - Final level key structure on api json
+  *  @returns {Number} - The arage number for the desired type of data from the weather pattern
+  */
 export const getAverageFromPattern = (data, typekey, patternkey) => {
   const patternSpread = [...data]
   return Math.round(patternSpread.map((pattern) => {
@@ -50,11 +63,21 @@ const allPatterns = map(
     }
   })
 
+/**
+  *  Takes the json api data and group the list of weather patterns by day:
+  *  @param {array} data - A list of all the weather patterns
+  *  @returns {object} - An object with weather patterns grouped within their respective dates as keys in YYYY-MM-DD format
+  */
 export const arrangePatternsByDays = (data) => {
   const list = [...data]
   return groupBy(allPatterns(list), 'dt_txt_pureday')
 }
 
+/**
+  *  Redistribute data for the day data populating each day with its respective weather patterns, given:
+  *  @param {object} data - object
+  *  @returns {object} - Addional Current information on the current day along with the weather patterns for that day
+  */
 export const getDaysData = (data) => {
   return Object.keys(data).map((day, index)=>{
     const currentWeather = { ...data[day][0].weather[0] }
@@ -78,6 +101,41 @@ export const getDaysData = (data) => {
       icon, avgTemp, windDirection, currentWeather, currentWeatherIcon, currentWeatherDescription, currentTemp
     }
   })
+}
+
+/**
+  *  Calculate the air density, given:
+  *  @param {number} temp - temperature (celsius)
+  *  @param {number} press - actual air pressure (millibars, i.e., hectopascals)
+  *  @param {number} dew - dew point (celsius)
+  *  @returns {number} - Air Density
+  */
+export const calculateRho = (temp, press, dew) => {
+  const c0 = 0.99999683
+  const c1 = -0.90826951E-02
+  const c2 = 0.78736169E-04
+  const c3 = -0.61117958E-06
+  const c4 = 0.43884187E-08
+  const c5 = -0.29883885E-10
+  const c6 = 0.21874425E-12
+  const c7 = -0.17892321E-14
+  const c8 = 0.11112018E-16
+  const c9 = -0.30994571E-19
+  const p = c0 + dew * (c1 + dew * (c2 + dew * (c3 + dew * (c4 + dew * (c5 + dew * (c6 + dew * (c7 + dew * (c8 + dew * (c9)))))))))
+  const psat_mbar = 6.1078 / (Math.pow(p, 8))
+
+  // Step 2: calculate the vapor pressure.
+  const pv_pascals = psat_mbar * 100.0
+
+  // Step 3: calculate the pressure of dry air, given the vapor
+  // pressure and actual pressure.
+  const pd_pascals = (press * 100) - pv_pascals
+
+  // Step 4: calculate the air density, using the equation for
+  // the density of a mixture of dry air and water vapor.
+  const density = (pd_pascals / (287.0531 * (temp + 273.15))) + (pv_pascals / (461.4964 * (temp + 273.15)))
+
+  return density
 }
 
 const reducer = (state = appInitialState, action) => {
